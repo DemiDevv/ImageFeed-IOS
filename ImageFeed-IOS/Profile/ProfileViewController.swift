84afcbaa2f865cf60019fb095441a20ea7551351
Demian Petropavlov <demyanpain@gmail.com>
2024-06-16 13:44:09 +0400

    Добавлен TabBar

---
+++ ImageFeed-IOS/Profile/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed-IOS
+//
+//  Created by Demain Petropavlov on 14.06.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBAction func buttonIsClicked(_ sender: Any) {
+    }
+}
+
