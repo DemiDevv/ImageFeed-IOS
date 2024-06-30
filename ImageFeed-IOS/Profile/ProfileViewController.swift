9097651da955c23fbaceb77e496b2a9ce2458a96
Demian Petropavlov <demyanpain@gmail.com>
2024-06-30 19:49:13 +0400

    Сверстан кодом экран Profile и выполнены все задачи спринта 9

--- ImageFeed-IOS/Profile/ProfileViewController.swift
+++ ImageFeed-IOS/Profile/ProfileViewController.swift
@@ -1,20 +1,67 @@
-//
-//  ProfileViewController.swift
-//  ImageFeed-IOS
-//
-//  Created by Demain Petropavlov on 14.06.2024.
-//
-
 import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var logoutButton: UIButton!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var loginLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var avatarImageView: UIImageView!
-    @IBAction func buttonIsClicked(_ sender: Any) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileViewCreated()
+    }
+    
+    func profileViewCreated() {
+        let imageViewProfile = UIImageView()
+        imageViewProfile.image = UIImage(named: "PhotoImage")
+        imageViewProfile.tintColor = .red
+        imageViewProfile.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageViewProfile)
+        imageViewProfile.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageViewProfile.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageViewProfile.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        imageViewProfile.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        
+        let exitButton = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        exitButton.setImage(UIImage(named: "ExitImage"), for: .normal)
+        exitButton.tintColor = .ypRedIOS
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exitButton)
+        exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24).isActive = true
+        exitButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        exitButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        exitButton.centerYAnchor.constraint(equalTo: imageViewProfile.centerYAnchor).isActive = true
+        
+        let fioLabel = UILabel()
+        fioLabel.text = "Екатерина Новикова"
+        fioLabel.textColor = .ypWhiteIOS
+        fioLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        fioLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(fioLabel)
+        fioLabel.topAnchor.constraint(equalTo: imageViewProfile.bottomAnchor, constant: 8).isActive = true
+        fioLabel.leadingAnchor.constraint(equalTo: imageViewProfile.leadingAnchor).isActive = true
+        
+        let userNameLabel = UILabel()
+        userNameLabel.text = "@ekaterina_nov"
+        userNameLabel.textColor = .ypGrayIOS
+        userNameLabel.font = .systemFont(ofSize: 13)
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userNameLabel)
+        userNameLabel.topAnchor.constraint(equalTo: fioLabel.bottomAnchor, constant: 8).isActive = true
+        userNameLabel.leadingAnchor.constraint(equalTo: imageViewProfile.leadingAnchor).isActive = true
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = .ypWhiteIOS
+        descriptionLabel.font = .systemFont(ofSize: 13)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        descriptionLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: imageViewProfile.leadingAnchor).isActive = true
+    }
+    
+    @objc private func didTapButton() {
+        
     }
 }
 
