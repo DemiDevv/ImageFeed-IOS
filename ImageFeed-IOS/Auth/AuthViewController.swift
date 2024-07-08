d55485d5b519f7e17d4706bfbc6d50b7ea081568
Demian Petropavlov <demyanpain@gmail.com>
2024-07-08 16:46:33 +0400

    Добавлен Progress View

---
+++ ImageFeed-IOS/Auth/AuthViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+final class AuthViewController: UIViewController {
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
+    
+    private func configureBackButton() {
+        navigationController?.navigationBar.backIndicatorImage = UIImage(named: "nav_back_button")
+        navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "nav_back_button")
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem?.tintColor = UIColor(named: "YP Black (iOS)")
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowWebViewSegueIdentifier {
+            guard
+                let webViewViewController = segue.destination as? WebViewViewController
+            else { fatalError("Failed to prepare for \(ShowWebViewSegueIdentifier)") }
+            webViewViewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackButton()
+    }
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        //TODO: process code
+    }
+
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+        dismiss(animated: true)
+    }
+}
---
+++ ImageFeed-IOS/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey = "CEryCnXH3ayPotJAzlhz3kFZveRcxNTlNB60nKlelG4"
+let secretKey = "ESQKfWSaxeg_mPBiruMLJqetq-pakEYgvTo6Z5PE7BU"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
